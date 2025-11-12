ce5d21f10d8db8d21b4a3e79e14e646ef577bffe
jpserignolli <jpserig@gmail.com>
2025-11-12 11:20:03 -0300

    UPDATE: Primeira Letra Maiúscula

--- script.js
+++ script.js
@@ -9,4 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("btnMinuscula").addEventListener("click", function () {
         resultado.innerHTML = input.value.toLowerCase();
     });
+
+    document.getElementById("btnPrimeiraLetra").addEventListener("click", function () {
+        let texto = input.value.trim();
+        let palavras = texto.split(" ");
+        let resultadoFinal = [];
+
+        for (let palavra of palavras) {
+            if (palavra.length === 0) continue;
+
+            
+            let primeiraLetra = palavra.charAt(0).toUpperCase();
+            let resto = palavra.slice(1).toLowerCase();
+
+            resultadoFinal.push(primeiraLetra + resto);
+        }
+
+        
+        resultado.innerHTML = resultadoFinal.join(" ");
+    });
 });
